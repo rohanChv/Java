3d95e42e8fc9e4db2bd14fcb66545a9985a18082
Rohan Chavan <rohangchavan@gmail.com>
2023-11-04 10:22:07 +0530

    First Commit on Github Java Day1

---
+++ Day1/Array/src/Array.java
@@ -0,0 +1,25 @@
+public class Array {
+    public static void main(String[] args) {
+        int[] singleDimensionalArray=new int[10];
+        int[][] twoDimensionalArray=new int[5][5];
+
+        for(int i=0;i<10;i++){
+            singleDimensionalArray[i]=i;
+            System.out.println(singleDimensionalArray[i]);
+        }
+        System.out.println();
+
+        for(int i=0;i<5;i++){
+            for(int j=0;j<i+1;j++){
+                twoDimensionalArray[i][j]=j;
+            }
+        }
+        for(int i=0;i<5;i++){
+            for(int j=0;j<i+1;j++){
+                System.out.print(twoDimensionalArray[i][j]);
+            }
+            System.out.println();
+        }
+
+    }
+}
