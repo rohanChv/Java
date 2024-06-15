fd50879095026bd0af86980ee72c5895f5f7971b
Rohan Gorakhnath Chavan <rohangchavan@gmail.com>
2024-06-15 15:07:26 +0530

    Added Array in package

--- Packages/src/PackageDemo.java
+++ Packages/src/PackageDemo.java
@@ -1,9 +1,13 @@
-
 import static com.java.MyFirstPackage.JavaPackage.*;
 
 public class PackageDemo {
     public static void main(String[] args) {
+        int[] array=new int[20];
         printStatement();
         printStatementWithName("Champaklal Jayantilal Gada");
+        for(int i=0;i<array.length;i++){
+            array[i]=i+1;
+        }
+        printArray(array,array.length);
     }
 }
--- Packages/src/com/java/MyFirstPackage/JavaPackage.java
+++ Packages/src/com/java/MyFirstPackage/JavaPackage.java
@@ -6,4 +6,10 @@ public class JavaPackage {
     public static  void printStatementWithName(String name){
         System.out.println("Hello "+name);
     }
+    public static void printArray(int array[],int size){
+        System.out.println("Printing Single Dimension Array");
+        for (int i=0;i<size;i++){
+            System.out.println(array[i]);
+        }
+    }
 }
