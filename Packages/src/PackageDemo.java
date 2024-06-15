33617f394868fc2238a90847ef11cf7df105157f
Rohan Gorakhnath Chavan <rohangchavan@gmail.com>
2024-06-15 14:58:50 +0530

    Packages Added

---
+++ Packages/src/PackageDemo.java
@@ -0,0 +1,9 @@
+
+import static com.java.MyFirstPackage.JavaPackage.*;
+
+public class PackageDemo {
+    public static void main(String[] args) {
+        printStatement();
+        printStatementWithName("Champaklal Jayantilal Gada");
+    }
+}
---
+++ Packages/src/com/java/MyFirstPackage/JavaPackage.java
@@ -0,0 +1,9 @@
+package com.java.MyFirstPackage;
+public class JavaPackage {
+    public static void printStatement(){
+        System.out.println("Hello");
+    }
+    public static  void printStatementWithName(String name){
+        System.out.println("Hello "+name);
+    }
+}
